e58d6d96b8a113e59fe1161e0fff88a3ce9635b5
Norbert Nopper <nopper@ux3d.io>
2022-04-28 09:40:36 +0200

    Updated script.

--- clip2usdz.py
+++ clip2usdz.py
@@ -1,14 +1,17 @@
 import os
 
-from pxr import Usd, UsdGeom, UsdShade
+from pxr import Sdf, Usd, UsdGeom, UsdShade
 
-# Patameters
+#
+# Parameters
+#
 
 endTimeCode = 143.5
 timeCodesPerSecond = 120.0
 
- 
+# 
 # Stage
+#
 
 stage = Usd.Stage.CreateNew('untitled.usda')
 
@@ -18,21 +21,50 @@ untitled = UsdGeom.Xform.Define(stage, '/untitled')
 
 scope = UsdGeom.Scope.Define(stage, '/untitled/Materials')
 
+#
 # Material
+#
 
 material = UsdShade.Material.Define(stage, '/untitled/Materials/material0')
 
+#
 # Shaders
-
-pbr_shader = UsdShade.Shader.Define(stage, '/untitled/Materials/material0/pbr_shader')
+#
 
 uvset0 = UsdShade.Shader.Define(stage, '/untitled/Materials/material0/uvset0')
 
+uvset0.CreateIdAttr("UsdPrimvarReader_float2")
+uvset0.CreateInput('varname',Sdf.ValueTypeNames.Token).Set('st0')
+
+#
+
 tex_emissive = UsdShade.Shader.Define(stage, '/untitled/Materials/material0/tex_emissive')
+
+tex_emissive.CreateIdAttr("UsdUVTexture")
+tex_emissive.CreateInput('file', Sdf.ValueTypeNames.Asset).Set("0/image0_lin.jpg")
+tex_emissive.CreateInput("st", Sdf.ValueTypeNames.Float2).ConnectToSource(uvset0.ConnectableAPI(), 'result')
+
+#
 
 tex_opacity = UsdShade.Shader.Define(stage, '/untitled/Materials/material0/tex_opacity')
 
+tex_opacity.CreateIdAttr("UsdUVTexture")
+tex_opacity.CreateInput('file', Sdf.ValueTypeNames.Asset).Set("0/image0_unlit_a.png")
+tex_opacity.CreateInput("st", Sdf.ValueTypeNames.Float2).ConnectToSource(uvset0.ConnectableAPI(), 'result')
+
+#
+
+pbr_shader = UsdShade.Shader.Define(stage, '/untitled/Materials/material0/pbr_shader')
+
+pbr_shader.CreateIdAttr("UsdPreviewSurface")
+pbr_shader.CreateInput("metallic", Sdf.ValueTypeNames.Float).Set(0.0)
+pbr_shader.CreateInput("roughness", Sdf.ValueTypeNames.Float).Set(1.0)
+
+material.CreateSurfaceOutput().ConnectToSource(pbr_shader.ConnectableAPI(), "surface")
+
+#
 # Stage settings
+#
 
 prim = stage.GetPrimAtPath('/untitled')
 stage.SetDefaultPrim(prim)
@@ -43,7 +75,9 @@ stage.SetTimeCodesPerSecond(timeCodesPerSecond)
 
 stage.SetMetadata('upAxis', 'Y')
 
+#
 # Saving
+#
 
 stage.GetRootLayer().Save()
 
